8ba2ae422f3502299f4d5b00fd643c498d85f152
Grace Hanson <hansongrace2004@gmail.com>
2025-08-20 22:05:07 -0400

    Create repository and controller for db

---
+++ src/main/java/com/grace/bus_delay_tracker/controller/RouteController.java
@@ -0,0 +1,4 @@
+package com.grace.bus_delay_tracker.controller;
+
+public class RouteController {
+}
---
+++ src/main/java/com/grace/bus_delay_tracker/repository/RouteRepository.java
@@ -0,0 +1,4 @@
+package com.grace.bus_delay_tracker.repository;
+
+public class RouteRepository {
+}
