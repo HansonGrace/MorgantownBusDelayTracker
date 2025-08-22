2e2fae2c4b886247811c4e5c580ec52c9268ef12
Grace Hanson <hansongrace2004@gmail.com>
2025-08-22 13:38:20 -0400

    Set up frontend and write out homepage idea in jsx file

---
+++ src/main/java/com/grace/bus_delay_tracker/frontend/homepage/homepage.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import './homepage.css'
+
+
+function Home() {
+    //need to do:
+    /* check if user is already logged in or not
+    *  if not, remove some buttons and show login button in top right corner
+    *  if logged in, show logout button and user info
+    **/
+
+    /* Home page layout ideas
+    * green wv state logo in middle
+    *  below OR above it, show title "Morgantown Bus Delays", with a small description under it
+    *
+    * Maybe a map of motown as the main image, or the mountainline bus, remember to make it dark
+    * Green and white theme idea
+    *  */
+}
