c4151748b90cd1da112aca1bab7f2179467cb1e5
Lauren Taylor <laurenktaylor04@gmail.com>
2025-09-10 21:15:58 -0400

    about page

--- src/frontend/src/pages/homepage/about.jsx
+++ src/frontend/src/pages/homepage/about.jsx
@@ -13,10 +13,18 @@ function About() {
                 </ul>
             </nav>
 
-            <div className="about">
+            <div className="title">
                 <h1>About</h1>
             </div>
 
+            <div className="about-container">
+                <p>The Morgantown Bus Delay Tracker was created for users of the Mountain Line Transit Authority (MLTA) 
+                    to keep track of different transit line delays. Users are able to go on this website to submit and cancel
+                    known delays. This way, users of the MLTA can stay up to date on the progress of all the lines so they are 
+                    not out of the loop.
+                </p>
+                <p>This website was created by Grace Hanson with frontend assistant Lauren Taylor.</p>
+            </div>
 
         </div>
     );
